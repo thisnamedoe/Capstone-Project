ec248e6d73454a70aad05e4755afdb02f12e83c1
Bill Zang <35152749+thisnamedoe@users.noreply.github.com>
2019-11-13 22:16:33 -0500

    users and migrations

--- backend/src/menus/models.py
+++ backend/src/menus/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
-class Menu(models.Model):
-    id = models.AutoField(primary_key=True)
-    restaurant_name = models.CharField(max_length=255)
-    food_items = models.ManyToManyField(MenuItem)
 
 class MenuItem(models.Model):
 	id = models.AutoField(primary_key=True)
 	name = models.CharField(max_length=255, null=False)
 	description = models.CharField(max_length=255, null=True)
 	price = models.IntegerField(null=False)
-	image = models.ImageField(max_length=255, null=True)
+	image = models.CharField(max_length=255, null=True)
+
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)
+    restaurant_name = models.CharField(max_length=255)
+    food_items = models.ManyToManyField(MenuItem)
--- backend/src/users/urls.py
+++ backend/src/users/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('create/', views.create),
-        path('email/', view.send_email)
+        # path('create/', views.create),
+        # path('email/', view.send_email)
         ]
