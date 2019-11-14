2e770ef490d7a545b216bd476ae38e2bfb0daf16
Bill Zang <35152749+thisnamedoe@users.noreply.github.com>
2019-11-13 22:22:04 -0500

    create custom user

--- backend/src/users/models.py
+++ backend/src/users/models.py
@@ -2,17 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
 
-
-
 class CustomUserManager(BaseUserManager):
-    """
-    Custom user model manager where email is the unique identifiers
-    for authentication instead of usernames.
-    """
     def create_user(self, email, password, **extra_fields):
-        """
-        Create and save a User with the given email and password.
-        """
         if not email:
             raise ValueError('The Email must be set')
         email = self.normalize_email(email)
@@ -22,15 +13,11 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_restaurant_user(self, email, password, **extra_fields):
-        """
-        Create and save a SuperUser with the given email and password.
-        """
         extra_fields.setdefault('is_restaurant', True)
-
         if extra_fields.get('is_restaurant') is not True:
-            raise ValueError('is_restaurant must have is_restaurant=True.')
+            raise ValueError('must have is_restaurant=True.')
         return self.create_user(email, password, **extra_fields)
-        
+
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
