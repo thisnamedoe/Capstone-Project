3590ebbfd78a12999c92cd5be509ff617a3e6230
paulyhong2000 <paulyhong2000@gmail.com>
2020-01-23 16:00:22 -0500

    initial work

--- backend/src/menus/views.py
+++ backend/src/menus/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from users.models import Menu, MenuItem
 
 # Create your views here.
+def create(request):
+    _restaurant_name = request.POST.get('restaurant_name')
+    _description = request.POST.get('description')
+
+    M = Menu(restaurant_name=_restaurant_name, food_items=_description)
+
+def get(request):
+    pass
+
+def delete(request):
+    pass
+
+def addmenuitem(request):
+    pass
+
+def removemenuitem(request):
+    pass
