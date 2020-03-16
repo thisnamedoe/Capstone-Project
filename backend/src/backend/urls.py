c61af63fd278e4688d342fbd9ea85cec22d04007
paulyhong2000 <paulyhong2000@gmail.com>
2020-03-16 16:10:09 -0400

    order tracking

--- backend/src/backend/urls.py
+++ backend/src/backend/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('menu/', include('menus.urls'))
+    path('menu/', include('menus.urls')),
+    path('order/', include('order.urls'))
 ]
