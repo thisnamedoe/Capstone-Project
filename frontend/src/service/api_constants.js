763d6bfdffc21d7acc6c738467dae6490ffb9941
Bill Zang <35152749+thisnamedoe@users.noreply.github.com>
2020-03-16 13:12:23 -0400

    Update api_constants.js

--- frontend/src/service/api_constants.js
+++ frontend/src/service/api_constants.js
@@ -1,7 +1,7 @@
-export const API_BASE = 'http://192.168.2.34:3000';
+export const API_BASE = 'http://127.0.0.1:8000';
 
-export const LOGIN_URL = '/auth/login';
-export const REGISTER_URL = '/auth/register';
+export const LOGIN_URL = '/users/authenticate';
+export const REGISTER_URL = '/users/create';
 export const RESTAURANT_URL = '/restaurant';
 export const RESTAURANT_FILTER_URL = '/restaurant/filter';
 export const FOOD_INFO_URL = '/food';
